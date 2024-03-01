f4e43df95982097cfb592fd6d2b813a274870fb8
Hoang Dinh Quang <hoang.dinh.quang@mediastep.com>
2024-03-01 17:57:27 +0700

    route

--- front-end/admin-web/src/pages/staff/route.js
+++ front-end/admin-web/src/pages/staff/route.js
@@ -2,7 +2,8 @@ import StaffPage from '.'
 import { GroupFill } from 'constants/icons.constants'
 import { PermissionKeys } from 'constants/permission-key.constants'
 import i18n from 'utils/i18n'
-
+import { CreateNewStaff } from './create-new-staff/create-new-staff.page'
+import { EditStaff } from './edit-staff/edit-staff.page'
 const { t } = i18n
 // Define the route
 const route = [
@@ -17,7 +18,32 @@ const route = [
     auth: true,
     permission: 'public',
     component: StaffPage,
-    child: []
+    child: [
+      {
+        key: 'app.staff.create-new',
+        focus: 'app.staff',
+        position: 4,
+        path: '/staff/create-new',
+        isMenu: false,
+        exact: true,
+        auth: true,
+        permission: PermissionKeys.ADMIN,
+        component: CreateNewStaff,
+        child: []
+      },
+      {
+        key: 'app.staff.edit-staff',
+        focus: 'app.staff',
+        position: 4,
+        path: '/staff/edit/:id',
+        isMenu: false,
+        exact: true,
+        auth: true,
+        permission: PermissionKeys.ADMIN,
+        component: EditStaff,
+        child: []
+      }
+    ]
   }
 ]
 export default route
