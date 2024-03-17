4e5ebd2f2acb22a01fed0cbd9524a8de974f0699
quanghoang0403 <quanghoang0403@gmail.com>
2024-03-17 13:38:20 +0700

    update

--- front-end/store-web/pages/index.tsx
+++ front-end/store-web/pages/index.tsx
@@ -4,16 +4,27 @@ import HomeCarousel from '@/components/Home/HomeCarousel'
 import ProductList from '@/components/ProductList'
 
 export default function HomePage() {
-  const promoProducts: IProduct[] = [
-    { name: 'Product 1', url: 'https://example.com/product1', price: 10 },
-    { name: 'Product 2', url: 'https://example.com/product2', price: 20 },
-    { name: 'Product 3', url: 'https://example.com/product3', price: 30 },
-  ];
+  const promoProduct: IProduct[] = [
+    { name: 'Basic Tee Long Sleeves Blue', url: '/imgs/productPromo/1.jpg', price: 180000 },
+    { name: 'Basic Tee Long Sleeves Black', url: '/imgs/productPromo/2.jpg', price: 200000 },
+    { name: 'Basic Tee Long Sleeves Striped', url: '/imgs/productPromo/3.jpg', price: 220000 },
+  ]
+  const productHighlight: IProduct[] = [
+    { name: 'Basic Tee With Long Sleeves Red', url: '/imgs/productHighlight/Basic Tee With Long Sleeves Red.jpg', price: 120000 },
+    { name: 'Classic Short Sleeves Shirt', url: '/imgs/productHighlight/Classic Short Sleeves Shirt.jpg', price: 120000 },
+    { name: 'Paris Long Tee', url: '/imgs/productHighlight/Paris Long Tee.jpg', price: 120000 },
+    { name: 'Paris Shirt', url: '/imgs/productHighlight/Paris Shirt.jpg', price: 120000 },
+    { name: 'Paris Tee', url: '/imgs/productHighlight/Paris Tee.jpg', price: 120000 },
+    { name: 'Striped Shirt', url: '/imgs/productHighlight/Striped Shirt.jpg', price: 120000 },
+    { name: 'Winter-Striped Tee Dress Black', url: '/imgs/productHighlight/Winter-Striped Tee Dress-black.jpg', price: 120000 },
+    { name: 'Winter-Striped Tee Dress White', url: '/imgs/productHighlight/Winter-Striped Tee Dress-white.jpg', price: 120000 },
+  ]
   return (
     <>
       <SEO title="Home Page" description="Describe the home page" />
       <HomeCarousel />
-      <ProductList />
+      <ProductList title="Top products" products={productHighlight} />
+      <ProductList title="Sale products" products={promoProduct} />
     </>
   )
 }
