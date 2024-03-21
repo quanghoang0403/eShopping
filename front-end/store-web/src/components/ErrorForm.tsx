964af256195f05e4eef4c9fc39357d44d6fa4c37
quanghoang0403 <quanghoang0403@gmail.com>
2024-03-22 00:18:14 +0700

    update sig in

---
+++ front-end/store-web/src/components/ErrorForm.tsx
@@ -0,0 +1,11 @@
+import { ErrorMessage } from '@hookform/error-message'
+import { FieldErrors } from 'react-hook-form'
+interface IProps {
+  name: string
+  errors: FieldErrors
+}
+
+export default function ErrorForm(props: IProps) {
+  const { name, errors } = props
+  return <ErrorMessage errors={errors} name={name} render={({ message }) => <p className="text-red-500 pt-2 error">{message}</p>} />
+}
