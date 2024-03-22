9f2d0557723ba74f7d361e53b49956f95d9d195e
quanghoang0403 <quanghoang0403@gmail.com>
2024-03-22 23:19:25 +0700

    update

--- front-end/admin-web/.eslintrc.js
+++ front-end/admin-web/.eslintrc.js
@@ -1,47 +1,23 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: [
-    'standard',
-    'plugin:react/recommended'
-  ],
+  extends: ["prettier"],
   overrides: [
     {
       env: {
-        node: true
+        node: true,
       },
-      files: [
-        '.eslintrc.{js,cjs}'
-      ],
+      files: [".eslintrc.{js,cjs}"],
       parserOptions: {
-        sourceType: 'script'
-      }
-    }
+        sourceType: "script",
+      },
+    },
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: [
-    'react'
-  ],
-  rules: {
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-    'no-unused-vars': 'off',
-    'react/no-deprecated': 'off',
-    'no-void': 'off',
-    'no-undef': 'off',
-    'no-case-declarations': 'off',
-    'no-useless-constructor': 'off',
-    'no-prototype-builtins': 'off',
-    'react/display-name': 'off',
-    'array-callback-return': 'off',
-    'prefer-promise-reject-errors': 'off',
-    'no-useless-escape': 'off',
-    'brace-style': 'off',
-    camelcase: 'off'
-  }
-}
+  plugins: ["react"],
+};
