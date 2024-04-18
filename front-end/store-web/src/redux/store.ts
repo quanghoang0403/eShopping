7d2063eba87e6623971d2fae25ba0d7029f42270
quanghoang0403 <quanghoang0403@gmail.com>
2024-04-18 10:41:31 +0700

    update

--- front-end/store-web/src/redux/store.ts
+++ front-end/store-web/src/redux/store.ts
@@ -1,18 +1,44 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { persistedReducer } from './persistConfig'
 import { persistStore } from 'redux-persist'
+import counterReducer from './features/counterSlice'
+import sessionReducer from './features/sessionSlice'
 
 export const makeStore = () => {
   const store = configureStore({
     reducer: persistedReducer,
-    middleware: (getDefaultMiddleware) =>
+    middleware: (getDefaultMiddleware: any) =>
       getDefaultMiddleware({
-        serializableCheck: false, // Disable serializable check for easier integration
+        serializableCheck: false,
       }),
   })
   const persistor = persistStore(store)
   return { store, persistor }
 }
+
+// export const makeStore = (usePersist: boolean = true) => {
+//   const customMiddleware = (getDefaultMiddleware: any) =>
+//     getDefaultMiddleware({
+//       serializableCheck: false,
+//     })
+//   if (usePersist) {
+//     const store = configureStore({
+//       reducer: persistedReducer,
+//       middleware: customMiddleware,
+//     })
+//     const persistor = persistStore(store)
+//     return { store, persistor }
+//   } else {
+//     const store = configureStore({
+//       reducer: {
+//         counter: counterReducer,
+//         session: sessionReducer,
+//       },
+//       middleware: customMiddleware,
+//     })
+//     return { store }
+//   }
+// }
 
 export type AppStore = ReturnType<typeof makeStore>
 export type RootState = ReturnType<AppStore['store']['getState']>
