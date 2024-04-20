0a206b75f1459a7451e5dbab268719aac6132f8d
quanghoang0403 <quanghoang0403@gmail.com>
2024-04-20 10:51:43 +0700

    nextconffig

--- front-end/store-web/next.config.js
+++ front-end/store-web/next.config.js
@@ -5,7 +5,6 @@ const { use } = require('react')
 module.exports = withFonts({
   transpilePackages: [],
   reactStrictMode: true,
-  use: ['@svgr/webpack', 'url-loader'],
   // images: {
   //   domains: ['images.unsplash.com', 'tailwindui.com','lh3.googleusercontent.com'],
   // },
