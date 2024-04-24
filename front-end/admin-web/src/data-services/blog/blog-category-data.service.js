88a6b00d1f6d557d405a27df0bf45c95f3f0cba6
cuong <ndcuong912@gmail.com>
2024-04-24 17:00:14 +0700

    update blog category data service

--- front-end/admin-web/src/data-services/blog/blog-category-data.service.js
+++ front-end/admin-web/src/data-services/blog/blog-category-data.service.js
@@ -3,7 +3,12 @@ const controller = 'blogcategory'
 const createBlogCategoryAsync = data=>{
     return http.post(`/${controller}/create-blog-category`,data)
 }
+const getBlogCategoriesAsync = (data)=>{
+    const {pageNumber,pageSize,keySearch} = data
+    return http.get(`/${controller}/get-blog-categories?PageNumber=${pageNumber}&PageSize=${pageSize}&KeySearch=${keySearch||''}`)
+}
 const BlogCategoryDataService = {
-    createBlogCategoryAsync
+    createBlogCategoryAsync,
+    getBlogCategoriesAsync
 } 
 export default BlogCategoryDataService
