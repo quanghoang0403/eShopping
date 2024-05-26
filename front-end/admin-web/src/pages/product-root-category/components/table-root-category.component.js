0b0ce498c404bc7527d141f38b9d01c0f072822b
cuong <ndcuong912@gmail.com>
2024-05-26 12:21:22 +0700

    create table

---
+++ front-end/admin-web/src/pages/product-root-category/components/table-root-category.component.js
@@ -0,0 +1,12 @@
+export default function TableRootCategory() {
+    const tableSettings = {
+        pageSize: 20,
+        columns: [
+
+        ]
+    }
+    return (
+        <>
+        </>
+    )
+}
