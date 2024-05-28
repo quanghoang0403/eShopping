2d75629b910251cf3aa29054bbd4c2f156f3d4dc
cuong <ndcuong912@gmail.com>
2024-05-28 15:41:11 +0700

    model for product size category controller

--- back-end/eShopping.Models/ProductCategories/AdminProductSizeCategoryDetailModel.cs
+++ back-end/eShopping.Models/ProductCategories/AdminProductSizeCategoryDetailModel.cs
@@ -1,14 +1,14 @@
-﻿using System;
+﻿using eShopping.Models.Products;
+using System;
 using System.Collections.Generic;
 
-namespace eShopping.Models.Products
+namespace eShopping.Models.ProductCategories
 {
-    public class AdminProductSizeCategoryModel
+    public class AdminProductSizeCategoryDetailModel
     {
         public Guid Id { get; set; }
-
         public string Name { get; set; }
-
+        public int Priority { get; set; }
         public IEnumerable<AdminProductSizeModel> ProductSizes { get; set; }
     }
 }
