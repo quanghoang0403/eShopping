73e23789fed059fa1eeb95aac78ad65dd58d0c4b
cuong <ndcuong912@gmail.com>
2024-06-26 00:06:40 +0700

    update

--- front-end/admin-web/src/components/shop-change-active-status-button/shop-change-active-status-button.component.js
+++ front-end/admin-web/src/components/shop-change-active-status-button/shop-change-active-status-button.component.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-
+import './shop-change-active-status-button.component.scss'
 export default function ChangeStatusButton(props){
   const {value, onChange, onClick, className} = props
   const {t} = useTranslation()
@@ -10,19 +10,26 @@ export default function ChangeStatusButton(props){
     deactivate: t('product.deactivate')
   }
   useEffect(()=>{
+    console.log(value)
     setActivate(value ? pageData.deactivate : pageData.activate)
   },[value])
   return(
-    <button
-      className={`${activate === pageData.deactivate ? 'action-activate' : 'action-deactivate'} ${className}`}
-      onClick={() => {
-        onClick(value)
-        if(onChange){
-          onChange(activate === pageData.deactivate ? false : true)
-        }
-      }}
-    >
-      {activate}
-    </button>
+    // <button
+    //   className={`${activate === pageData.deactivate ? 'action-activate' : 'action-deactivate'} ${className}`}
+    //   onClick={() => {
+    //     onClick(value)
+    //     if(onChange){
+    //       onChange(activate === pageData.deactivate ? false : true)
+    //     }
+    //   }}
+    // >
+    //   {activate}
+    // </button>
+    <div className="checkbox-wrapper-63">
+      <label className="switch">
+        <input type="checkbox" onChange={()=>onChange(activate === pageData.deactivate ? true : false)} checked={activate === pageData.deactivate}/>
+        <span className="slider"></span>
+      </label>
+    </div>
   );
 }
